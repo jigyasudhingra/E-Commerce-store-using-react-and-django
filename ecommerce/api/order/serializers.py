ff19ae34e6a7d1c1fbb179a765528db51499ef40
Jigyasu Dhingra <jaidhingra987@outlook.com>
2020-11-26 20:12:58 +0530

    Revert "All Files"
    This reverts commit 2584b4647b9cedeaaa44c61eaac38153614b2839.

--- ecommerce/api/order/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import Order
-
-
-class OrderSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Order
-        fields = ('user')
